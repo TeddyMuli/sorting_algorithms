c1fb609f7629b63bff8a46fef6b2b2f462543253
root <root@MAMBA.localdomain>
2023-04-27 16:36:17 +0300

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size) {
+int i, j ,temp;
+
+for (i = 0; i < size; i++)
+{
+    for (j = i + 1; j < size; j++)
+    {
+        if (array[j] < array[i])
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            print_array(array,size);
+        }
+    }
+}
+}
