29c4e97fcab0ca09bc8ac4ccc0cd0968dc94976b
root <root@MAMBA.localdomain>
2023-04-27 18:14:12 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,21 +3,35 @@
 #include "sort.h"
 
 void bubble_sort(int *array, size_t size) {
-int temp;
-unsigned int i,j, n = size;
+int temp, new;
+unsigned int i,n = size;
 
+do {
+		new = 0;
 
-for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
+				new = i;
+				print_array(array, size);
+			}
+		}
+		n = new;
+	} while (n >= 1);}
+
+int main(void)
 {
-    for (j = i + 1; j < n; j++)
-    {
-        if (array[j] < array[i])
-        {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            print_array(array,size);
-        }
-    }
-}
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
