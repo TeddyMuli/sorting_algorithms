cd7c6de8aa54dba47c1fa62ff5040c2e812b2336
root <root@MAMBA.localdomain>
2023-04-27 18:28:14 +0300

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "sort.h"
 
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers,
+ * in ascending order using the Insertion sort algorithm
+ * @list: the list
+**/
 
 void insertion_sort_list(listint_t **list) {
     int i = 0;
