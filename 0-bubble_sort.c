49e2ffd0d99c4bab55f6d7250d5ded1911841c81
root <root@MAMBA.localdomain>
2023-04-27 18:15:02 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,17 +21,6 @@ do {
 			}
 		}
 		n = new;
-	} while (n >= 1);}
-
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	} while (n >= 1);
 }
+
